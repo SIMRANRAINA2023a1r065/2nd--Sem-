a8fdda785d7fa24cbeeb9bc20f63c9a5871bdc1f
SIMRANRAINA2023a1r065 <146851787+SIMRANRAINA2023a1r065@users.noreply.github.com>
2024-03-24 19:19:50 +0530

    Add files via upload

---
+++ (Input) Write a program to delete an element in an array.c
@@ -0,0 +1,25 @@
+// Write a program to delete an element in an array
+#include<stdio.h>
+void main()
+{
+int arr[30],i,num,loc;
+printf("Enter the size of an array:");
+scanf("%d",&num);
+printf("Enter the array elements:",num);
+for(i=0;i<num;i++)
+{
+scanf("%d",&arr[i]);
+}
+printf("Enter the location to delete the value:");
+scanf("%d",&loc);
+while(loc<num)
+{
+arr[loc-1]=arr[loc];
+loc++;
+}
+num--;
+for(i=0;i<num;i++)
+{
+printf("\n%d",arr[i]);
+}
+}
